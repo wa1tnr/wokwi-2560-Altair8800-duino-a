094db2318e1aee4d43cb0904f109e95dc3231768
wa1tnr <wa1tnr@users.noreply.github.com>
2022-09-06 20:33:16 +0000

    trivial change 6 Sep 2022
    offline and online now conform

--- wkwi/altr8800-aa/Altair8800.ino
+++ wkwi/altr8800-aa/Altair8800.ino
@@ -1,3 +1,8 @@
+
+    // please see README_NOW.md  - do not delay ;)
+
+// Tue  6 Sep 20:27:26 UTC 2022
+
 // -----------------------------------------------------------------------------
 // Altair 8800 Simulator
 // Copyright (C) 2017 David Hansel
